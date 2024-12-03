c19bb6383804bf550b7be63a1fb11f7916a1c5de
Pietro Sbaraccani <pietro.sbaraccani@studio.unibo.it>
2024-12-03 23:52:01 +0100

    Refactor LevelTask and update FSM documentation for EMPTYING state

--- src/smart_waste_disposal/tasks/LevelTask.cpp
+++ src/smart_waste_disposal/tasks/LevelTask.cpp
@@ -10,47 +10,56 @@
 #define T3 5000
 
 LevelTask::LevelTask(WasteDisposal *wasteDisposal, UserConsole *userConsole)
-    : wasteDisposal{wasteDisposal}, userConsole{userConsole} {
+    : wasteDisposal{wasteDisposal}, userConsole{userConsole}
+{
     setState(OK);
 }
 
 CleanPattern *cleanPattern = new CleanPattern();
 
-void LevelTask::tick() {
+void LevelTask::tick()
+{
     double level = wasteDisposal->getCurrentLevel();
-    switch (state) {
-        case OK: {
-            Logger.bin(String((int)((1 - level) * 100.0 / (1 - MAXDISTANCE))));
-            if (level <= MAXDISTANCE) {
-                wasteDisposal->setFull();
-                wasteDisposal->closeDoor();
-                userConsole->displayMessage("CONTAINER FULL");
-                setState(FULL);
-            }
-            break;
+    switch (state)
+    {
+    case OK:
+    {
+        Logger.bin(String((int)((1 - level) * 100.0 / (1 - MAXDISTANCE))));
+        if (level <= MAXDISTANCE)
+        {
+            wasteDisposal->setFull();
+            wasteDisposal->closeDoor();
+            userConsole->displayMessage("CONTAINER FULL");
+            setState(FULL);
         }
-        case FULL:
-            if (MsgService.isMsgAvailable(*cleanPattern) && level > MAXDISTANCE) {
-                Msg *msg = MsgService.receiveMsg(*cleanPattern);
-                setState(EMPTYING);
-                wasteDisposal->bendDoor();
-            }
-            break;
-        case EMPTYING:
-            if (elapsedTimeInState() > T3) {
-                wasteDisposal->closeDoor();
-                wasteDisposal->setAcceptingWaste();
-                setState(OK);
-            }
-            break;
+        break;
+    }
+    case FULL:
+        if (MsgService.isMsgAvailable(*cleanPattern) && level > MAXDISTANCE)
+        {
+            Msg *msg = MsgService.receiveMsg(*cleanPattern);
+            setState(EMPTYING);
+            wasteDisposal->bendDoor();
+        }
+        break;
+    case EMPTYING:
+        if (elapsedTimeInState() > T3)
+        {
+            wasteDisposal->closeDoor();
+            wasteDisposal->setAcceptingWaste();
+            setState(OK);
+        }
+        break;
     }
 }
 
-void LevelTask::setState(State s) {
+void LevelTask::setState(State s)
+{
     state = s;
     stateTimestamp = millis();
 }
 
-long LevelTask::elapsedTimeInState() {
+long LevelTask::elapsedTimeInState()
+{
     return millis() - stateTimestamp;
 }
